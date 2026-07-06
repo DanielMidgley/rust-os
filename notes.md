f8e4418846d5095e23769bfdb1fd038a359bc5f2
DanielMidgley <126402725+DanielMidgley@users.noreply.github.com>
2026-07-06 10:07:28 +0100

    Add testing

--- notes.md
+++ notes.md
@@ -3,5 +3,4 @@ cargo build
 cargo bootimage
 
 Boot in QEMU with
-qemu-system-x86_64 -drive format=raw,file=target/x86_64-rust-os/debug/bootimage-rust-os.bin
-C:\msys64\mingw64\bin\qemu-system-x86_64.exe -drive format=raw,file=target/x86_64-rust-os/debug/bootimage-rust-os.bin 
+cargo run
--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -5,27 +5,18 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use rust_os::println;
 
-#[unsafe(no_mangle)]
+#[unsafe(no_mangle)] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
+fn test_runner(tests: &[&dyn Fn()]) {
+    unimplemented!();
 }
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rust_os::test_panic_handler(info)
